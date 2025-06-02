a083d522f31ed704d65f464e08f6ed7c7581fdff
Dennisovich503 <dennispalacios86@gmail.com>
2025-06-02 08:32:02 -0600

    Ejercicio 6.3.1

---
+++ Ejercicio6.3.1.py
@@ -0,0 +1,62 @@
+'''
+Clase:        Clase 5
+Tema:         Uso de listas
+Ejercicio:    6.3.1
+Descripción:  Cree un programa que elimine los números repetidos de una lista
+
+Autor:        Dennis Alejandro Palacios López
+Fecha:        2025-05-30
+Estado:       Terminado 
+'''
+
+#Versión1
+numeros = input('Ingresa una lista de números(separa por espacio): ')
+    
+no_repetidos = []
+
+for n in numeros:
+    if n not in no_repetidos:
+        no_repetidos.append(n)
+    else:
+        no_repetidos.append('')
+        
+
+print('Lista con números repetidos:', end=' ')
+for n in numeros:
+    print(n, end='')
+
+print('\nLista de números sin repetir:', end=' ')
+for n in no_repetidos:
+    print(n, end='')
+
+
+
+'''
+numeros = []
+while True:
+    num = int(input('Ingresa el número: '))
+    numeros.append(num)
+    conf = input('¿Deseas seguir ingresando números? si/no: ')
+    if(conf == 'no'):
+        break
+    else:
+        pass
+    
+no_repetidos = []
+
+for n in numeros:
+    if n not in no_repetidos:
+        no_repetidos.append(n)
+        
+
+print('Lista con números repetidos:', end=' ')
+for n in numeros:
+    print(n, end=' ')
+
+print('\nLista de números sin repetir:', end=' ')
+for n in no_repetidos:
+    print(n, end=' ')
+'''   
+    
+    
+    
